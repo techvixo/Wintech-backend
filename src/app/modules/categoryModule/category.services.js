ee60d37011d5bc34e85cb0426138f29319a0ee76
Fahad Hossain <fahadhossaim24@gmail.com>
2024-10-24 11:28:16 +0600

    category services

---
+++ src/app/modules/categoryModule/category.services.js
@@ -0,0 +1,36 @@
+const Category = require('./category.model');
+
+// Service to create a new category
+const createCategory = async (data) => {
+  return await Category.create(data);
+};
+
+// Service to get all category
+const getAllCategory = async () => {
+  return await Category.find().sort("-createdAt");
+};
+
+// Service to get a specific category by ID
+const getSpecificCategory = async (id) => {
+  return await Category.findOne({ _id: id });
+};
+
+// Service to update a specific category
+const updateSpecificCategory = async (id, data) => {
+  return await Category.updateOne({ _id: id }, data, {
+    runValidators: true
+  });
+};
+
+// Service to delete a specific category
+const deleteSpecificCategory = async (id) => {
+  return await Category.deleteOne({ _id: id });
+};
+
+module.exports = {
+  createCategory,
+  getAllCategory,
+  getSpecificCategory,
+  updateSpecificCategory,
+  deleteSpecificCategory
+};
