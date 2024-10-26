14d57b53cb364a876b55b016dbcaa188a5ff5da2
Fahad Hossain <fahadhossaim24@gmail.com>
2024-10-26 16:47:56 +0600

    OurTeam model done

---
+++ src/app/modules/ourTeamModule/ourTeam.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+
+const ourTeamSchema = new mongoose.Schema({
+    name_en: {
+        type: String,
+        required: true
+    },
+    name_cn: {
+        type: String,
+        required: true
+    },
+    image: String,
+    role: {
+        type: String,
+        enum: {
+            values: ["Owner", "Admin", "Manager", "Employee"],
+            message: '{VALUE} is not accepted as role. You have to use Owner/Admin/Manager/Employee.'
+        },
+        required: true
+    },
+    experience_en: String,
+    experience_cn: String,
+    language: [String],
+    address_en: {
+        type: String,
+        required: true,
+    },
+    address_cn: {
+        type: String,
+        required: true,
+    },
+    university_en: {
+        type: String,
+        required: true
+    },
+    university_cn: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['active', 'disable']
+    },
+}, {
+    timestamps: true,
+})
+
+const OurTeam = mongoose.model('outTeam', ourTeamSchema);
+
+module.exports = OurTeam
