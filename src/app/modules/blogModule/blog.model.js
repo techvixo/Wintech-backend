3d927fd52fe2f824cdece91bbdba7c06466975a8
Fahad Hossain <fahadhossaim24@gmail.com>
2024-10-27 15:32:47 +0600

    [done] blog model

---
+++ src/app/modules/blogModule/blog.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+
+const mongooseSchema = new mongoose.Schema({
+    name_en: {
+        type: String,
+        required: true,
+    },
+    name_cn: {
+        type: String,
+        required: true,
+    },
+    description_en: {
+        type: String,
+        required: true,
+    },
+    description_cn: {
+        type: String,
+        required: true,
+    },
+    heading_image: String,
+    featured_images: [String],
+    author: {
+        name_en: String,
+        name_cn: String,
+        adminId: String,
+    },
+    status: {
+        type: Boolean,
+        default: true,
+    }
+}, {
+    timestamps: true
+})
+
+
+const Blog = mongoose.model('blog', mongooseSchema);
+module.exports = Blog
