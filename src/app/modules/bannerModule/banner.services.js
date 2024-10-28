1d1112097b88349e9ea237b42bd9f8af679a4d31
Fahad Hossain <fahadhossaim24@gmail.com>
2024-10-28 13:05:37 +0600

    banner services done

---
+++ src/app/modules/bannerModule/banner.services.js
@@ -0,0 +1,34 @@
+const Banner = require('./banner.model');
+
+// Service to create a new banner
+const createBanner = async (data) => {
+  return await Banner.create(data);
+};
+
+// Service to get all banners
+const getAllBanners = async () => {
+  return await Banner.find().sort('-createdAt');
+};
+
+// Service to get a specific banner by ID
+const getSpecificBanner = async (id) => {
+  return await Banner.findOne({ _id: id });
+};
+
+// Service to update a specific banner
+const updateSpecificBanner = async (id, data) => {
+  return await Banner.updateOne({ _id: id }, data, { runValidators: true });
+};
+
+// Service to delete a specific banner
+const deleteSpecificBanner = async (id) => {
+  return await Banner.deleteOne({ _id: id });
+};
+
+module.exports = {
+  createBanner,
+  getAllBanners,
+  getSpecificBanner,
+  updateSpecificBanner,
+  deleteSpecificBanner,
+};
